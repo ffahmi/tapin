055142bb0616ec980cd8b9212cba68e1fcd7505a
faizal fahmi mubarok <43566730+ffahmi@users.noreply.github.com>
2026-04-10 10:20:36 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0A93D8',
+        secondary: '#F1CC01',
+        tertiery: '#2F3946',
+      },
+    },
+  },
+  plugins: [],
+}
+
