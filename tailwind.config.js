950409388beab0e9df6c738bfe12f57e7721d15b
faizal fahmi mubarok <43566730+ffahmi@users.noreply.github.com>
2026-04-10 11:04:23 +0800

    update detail page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./detail-service.html"],
   theme: {
     extend: {
       colors: {
         primary: '#0A93D8',
         secondary: '#F1CC01',
-        tertiery: '#2F3946',
+        tertiery: '#2F3946'
       },
     },
   },
