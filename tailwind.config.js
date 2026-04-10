35d8ceb011ec7dfd6bddac1fa74438ad5a8d455b
faizal fahmi mubarok <43566730+ffahmi@users.noreply.github.com>
2026-04-10 14:51:29 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./detail-service.html"],
+  content: ["./index.html", "./detail-service.html", "./form-order.html", "./confirm-order.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#0A93D8',
+        primary:  '#2563eb',
         secondary: '#F1CC01',
-        tertiery: '#2F3946'
+        tertiery: '#2F3946',
+        fourth: '#2563eb',
+        background: '#F7F7F7',
+        stroke: '#E8E8E8',
+      },
+      fontFamily: {
+        sans: ['"Google Sans"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        serif: ['"EB Garamond"', 'ui-serif', 'Georgia', 'serif'],
       },
     },
   },
